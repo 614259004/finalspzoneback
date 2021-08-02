f27fea1b7901b6f788299055ff150f7ab65afa0d
unknown <614259013@webmail.npru.ac.th>
2021-08-02 21:51:07 +0700

    PromotionV2

--- app/Models/Promotion_Model.php
+++ app/Models/Promotion_Model.php
@@ -6,7 +6,16 @@ use CodeIgniter\Model;
 class Promotion_Model extends Model{
     protected $table = 'sp_promotion';
     protected $primaryKey = 'Pr_promotion_code';
-    protected $allowedFields =['Pr_promotion_code','Pr_time_begin','Pr_time_out','Pr_detail','Pr_sale','Pr_image','P_productid'];
+    protected $allowedFields =['Pr_promotion_code',
+                                'Pr_time_begin',
+                                'Pr_time_out',
+                                'Pr_detail',
+                                'Pr_sale',
+                                'Pr_image',
+                                'P_productid',
+                                'Pr_amountPro',
+                                'Pr_size',
+                                'Pr_status'];
 
 }
 
